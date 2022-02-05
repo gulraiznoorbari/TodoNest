d94c782b3f713a72b7a87e8fdd720fb8d24bd219
Gulraiz Noor Bari <gulraiznoorbari@gmail.com>
2022-02-05 21:09:34 +0500

    📦 NEW: Add User Authorization

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const auth = require("./routes/auth");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.get("/api", (req, res) => {
     res.send("Express app is running...");
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const validateUserInfo = require("../validation/registerValidation");
+const requireAuth = require("../middleware/permissions");
 
 // @route   GET /api/auth/test
 // @desc    Test the auth route
@@ -15,11 +17,11 @@ router.get("/test", (req, res) => {
 // @access  Public
 router.post("/register", async (req, res) => {
     try {
+        // validate user:
         const { errors, isValid } = validateUserInfo(req.body);
         if (!isValid) {
             return res.status(400).json(errors);
         }
-
         // Check for existing email:
         const existingEmail = await User.findOne({
             email: new RegExp("^" + req.body.email + "$", "i"),
@@ -27,18 +29,77 @@ router.post("/register", async (req, res) => {
         if (existingEmail) {
             return res.status(400).json({ error: "Email already exists!" });
         }
+        // hash the password:
         const hashPassword = await bcrypt.hash(req.body.password, 10);
+        // create new user:
         const newUser = new User({
             email: req.body.email,
             password: hashPassword,
             name: req.body.name,
         });
+        // save user to database:
         const savedUser = await newUser.save();
-        return res.json(savedUser);
+
+        // return the user without password (for security):
+        const userToReturn = { ...savedUser._doc };
+        delete userToReturn.password;
+
+        return res.json(userToReturn);
     } catch (error) {
-        console.log(error);
-        res.status(500).send(error.message);
+        return res.status(500).send(error.message);
     }
+});
+
+// @route   POST /api/auth/login
+// @desc    login user and return access token
+// @access  Public
+router.post("/login", async (req, res) => {
+    try {
+        // check for the user:
+        const checkUser = await User.findOne({
+            email: new RegExp("^" + req.body.email + "$", "i"),
+        });
+
+        if (!checkUser) {
+            return res.status(400).json({ error: "Login credentials invalid!" });
+        }
+
+        // check for the user password:
+        const passwordMatch = await bcrypt.compare(req.body.password, checkUser.password);
+        if (!passwordMatch) {
+            return res.status(400).json({ error: "Login credentials invalid!" });
+        }
+
+        // assign JWT & configure Cookie:
+        const payload = { userId: checkUser._id };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d" });
+        res.cookie("Access-Token", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 1000),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+        });
+
+        // return the user without password (for security):
+        const userToReturn = { ...checkUser._doc };
+        delete userToReturn.password;
+
+        return res.json({
+            token: token,
+            user: userToReturn,
+        });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+// @route   GET /api/auth/current
+// @desc    Return currently authorized user
+// @access  Private
+router.get("/current", requireAuth, (req, res) => {
+    if (!req.user) {
+        return res.status(401).send("Unauthorized");
+    }
+    return res.json(req.user);
 });
 
 module.exports = router;
